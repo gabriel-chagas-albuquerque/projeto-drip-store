1423e3f8ae9ed419d5e857038df434eead5e588a
SamuelTal <samueltelesvie@gmail.com>
2025-05-26 14:23:11 -0300

    Logo.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
+import Logo from "./Logo";
+
 const Header = () => {
     return (
-        <h1>Header</h1>
+      <>
+       <h1>Header</h1>
+       <Logo color={'#C92071'} />
+      </>
+       
       );
 }
  
