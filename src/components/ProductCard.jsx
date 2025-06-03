a0d281cff5969ded976610625bcc8e04393c1588
gabriel-chagas-albuquerque <gabrieldafortaleza0100@gmail.com>
2025-06-02 23:40:24 -0300

    Update ProductDetails.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,15 +1,18 @@
+import { Link } from "react-router-dom";
 
 const ProductCard = ({image, category, title, priceBefore, price}) => {
     return ( 
-        <div className="w-[163px] md:w-[292px]">
-           <img src={image} alt="Imagem produto" className="mb-[11px]" />
-           <h5 className="capitalize text-xs text-[#8F8F8F] lg:text-lg">{category}</h5>
-           <h3 className="text-sm  text-[#474747] lg:text-2xl text-ellipsis overflow-hidden whitespace-nowrap">{title}</h3>
-           <div className="flex gap-[8px] lg:text-2xl">
-               <h4 className="line-through text-[#8F8F8F]">{`$${priceBefore}`}</h4>
-               <h4 className="text-[#1F1F1F]">{`$${price}`}</h4>
-           </div>
-        </div>
+        <Link to={'products/product/:id'}>
+            <div className="w-[163px] md:w-[292px]">
+               <img src={image} alt="Imagem produto" className="mb-[11px]" />
+               <h5 className="capitalize text-xs text-[#8F8F8F] lg:text-lg">{category}</h5>
+               <h3 className="text-sm  text-[#474747] lg:text-2xl text-ellipsis overflow-hidden whitespace-nowrap">{title}</h3>
+               <div className="flex gap-[8px] lg:text-2xl">
+                   <h4 className="line-through text-[#8F8F8F]">{`$${priceBefore}`}</h4>
+                   <h4 className="text-[#1F1F1F]">{`$${price}`}</h4>
+               </div>
+            </div>
+        </Link>
      );
 }
  
--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,9 +1,9 @@
 
 const ProductDetails = () => {
     return ( 
-        <>
+        <main className="bg-[rgba(204,204,204)]">
         <h1>Product Details</h1>
-        </>
+        </main>
      );
 }
  
